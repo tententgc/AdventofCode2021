73fe6286c7bb4333f5a3919d8c0bfbbc0935dd01
tententgc <nyman3527@gmail.com>
2021-12-27 17:02:37 +0700

    update days1 and days 2

---
+++ Days2/day2.py
@@ -0,0 +1,27 @@
+values = [(e.split(" ")[0], int(e.split(" ")[1])) for e in open("./Days2/input.txt")]
+
+
+depth = 0; 
+horizontal = 0 
+
+for k,v in values:
+    if k == "forward":
+        horizontal += v 
+    else:
+        depth += v if k == "down" else -v 
+print (f"Part 1: {horizontal * depth}") 
+
+
+
+depth = 0
+horizontal = 0
+aim = 0
+
+for k, v in values:
+    if k == "forward":
+        horizontal += v
+        depth += aim * v
+    else:
+        aim += v if k == "down" else -v
+
+print(f"Part two {depth * horizontal}")
